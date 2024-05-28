1841605fdcd019613ee17510e20178ecd40a321a
UGBAH-NDUKA DAVID KING <130305040+Oche-007@users.noreply.github.com>
2024-05-28 15:29:08 +0100

    Interface class

---
+++ 4th Class/Java Interfaces/Storage.java
@@ -0,0 +1,11 @@
+public class Storage {
+    String type = "SSD";
+    String manufacturer = "Lenovo";
+    int capacity = 256;
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "storage";
+    }
+}
